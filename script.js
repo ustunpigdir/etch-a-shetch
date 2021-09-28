f69616404800303bb49013d8a930a075f008cc8b
ustunpigdir <76748413+ustunpigdir@users.noreply.github.com>
2021-09-28 18:04:25 +0300

    added hover effect

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.getElementById("container")
 
-
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const divCreator = document.createElement("div")
+        divCreator.className = "grid"
+        divCreator.addEventListener("mouseover", () => divCreator.style.backgroundColor = "black")
         container.appendChild(divCreator)
     }
 }
+
